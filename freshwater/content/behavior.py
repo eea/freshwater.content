8f693d9053ceb88b47895269c6c0c8f5273112b4
laszlocseh <csehlaszlo91@gmail.com>
2021-05-05 11:40:04 +0300

    Updated Dashboard metadata fields

--- freshwater/content/behavior.py
+++ freshwater/content/behavior.py
@@ -11,14 +11,17 @@ from .interfaces import ICatalogueMetadata  #, IExternalLinks
 
 
 class CatalogueMetadata(MetadataBase):
-    """Wise metadata"""
-
+    """Freshwater metadata"""
+    title = DCFieldProperty(ICatalogueMetadata["title"])
+    description = DCFieldProperty(ICatalogueMetadata["description"])
+    lineage = DCFieldProperty(ICatalogueMetadata["lineage"])
     original_source = DCFieldProperty(ICatalogueMetadata["original_source"])
     organisation = DCFieldProperty(ICatalogueMetadata["organisation"])
     dpsir_type = DCFieldProperty(ICatalogueMetadata["dpsir_type"])
-    theme = DCFieldProperty(ICatalogueMetadata["theme"])
-    subtheme = DCFieldProperty(ICatalogueMetadata["subtheme"])
+    topic = DCFieldProperty(ICatalogueMetadata["topic"])
+    tags = DCFieldProperty(ICatalogueMetadata["tags"])
     publication_year = DCFieldProperty(ICatalogueMetadata["publication_year"])
+    temporal_coverage = DCFieldProperty(ICatalogueMetadata["temporal_coverage"])
+    geo_coverage = DCFieldProperty(ICatalogueMetadata["geo_coverage"])
+    data_source_info = DCFieldProperty(ICatalogueMetadata["data_source_info"])
     thumbnail = DCFieldProperty(ICatalogueMetadata["thumbnail"])
-
-    sources = DCFieldProperty(ICatalogueMetadata["sources"])
--- freshwater/content/interfaces.py
+++ freshwater/content/interfaces.py
@@ -1,5 +1,6 @@
 """Module where all interfaces, events and exceptions live."""
 
+from plone.app.dexterity import _
 from plone.app.textfield import RichText
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.namedfile.field import NamedBlobImage
@@ -42,9 +43,30 @@ class ICatalogueMetadata(model.Schema):
     website and SDI catalogue
     """
 
+    # default fieldset
+    title = TextLine(
+        title=_(u'label_title', default=u'Title'),
+        required=True
+    )
+
+    description = Text(
+        title=_(u'label_description', default=u'Description'),
+        description=_(
+            u'help_description',
+            default=u'Used in item listings and search results.'
+        ),
+        required=True,
+    )
+
+    lineage = Text(
+        title=u"Lineage",
+        required=False,
+    )
+
     original_source = TextLine(
         title=u"Original source",
-        description=u"If EEA link, can trigger automatic fetching of EEA information",
+        description=u"If EEA link, can trigger "
+                    u"automatic fetching of EEA information",
     )
 
     organisation = Choice(
@@ -58,17 +80,26 @@ class ICatalogueMetadata(model.Schema):
         title=u"DPSIR", required=False, vocabulary="wise_dpsir_vocabulary"
     )
 
-    theme = Choice(title=u"Theme", required=False, vocabulary="wise_themes_vocabulary")
+    # theme = Choice(title=u"Theme", required=False,
+    #                vocabulary="wise_themes_vocabulary")
 
-    subtheme = Choice(
-        title=u"Subtheme", required=False, vocabulary="wise_subthemes_vocabulary"
-    )
+    topic = Text(title=u"Topic", required=False)
+
+    # subtheme = Choice(title=u"Subtheme", required=False,
+    #                   vocabulary="wise_subthemes_vocabulary")
+
+    tags = Text(title=u"Tags", required=False)
 
     publication_year = Int(title=u"Publication year", required=True)
+
+    temporal_coverage = TextLine(title=u"Temporal coverage", required=False)
+
+    geo_coverage = TextLine(title=u"Geographical coverage", required=False)
+
+    data_source_info = RichText(title=u"Data source information",
+                                description=u"", required=False)
 
     thumbnail = NamedBlobImage(
         title=u"Preview image (thumbnail)",
         required=False,
     )
-
-    sources = RichText(title=u"Sources", description=u"", required=False)
