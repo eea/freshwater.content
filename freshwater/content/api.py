eb2076c6fe9c44a730b30c4d14b6914a0259fc46
laszlocseh <csehlaszlo91@gmail.com>
2021-09-24 15:41:59 +0300

    Added api to modify a bookmark

--- freshwater/content/api.py
+++ freshwater/content/api.py
@@ -1,7 +1,10 @@
 from collective.bookmarks.api.utils import bookmark_dict_to_json_dict
+from collective.bookmarks.api.utils import get_bookmark_from_request
 from collective.bookmarks.api.utils import get_owner
 from collective.bookmarks.storage import Bookmarks
+from plone.protect.interfaces import IDisableCSRFProtection
 from plone.restapi.services import Service
+from zope.interface import alsoProvides
 from repoze.catalog.query import Eq, NotEq
 from zExceptions import NotFound
 
@@ -19,12 +22,16 @@ class BookmarksAll(Bookmarks):
 
 class BookmarksGet(Service):
     def reply(self):
-        """get all bookmark
+        """get all bookmarks
 
         """
 
         bookmark = BookmarksAll()
-        owner = get_owner(request=self.request)
+        try:
+            owner = get_owner(request=self.request)
+        except:
+            owner = '__anonym__'
+
         query_eq = Eq("owner", owner)
         query_noteq = NotEq("owner", owner)
         bookmarks_eq = bookmark.fetch_all(query_eq)
@@ -35,3 +42,28 @@ class BookmarksGet(Service):
             return [bookmark_dict_to_json_dict(x) for x in bookmarks]
         
         raise NotFound("No such bookmark found.")
+
+
+class BookmarkPut(Service):
+    def reply(self):
+        """update bookmark by
+
+        uid
+        owner
+        group
+        queryparams (optional): serialized querystring
+
+        Add new bookmark if bookmark not found.
+        """
+        owner, uid, group, queryparams, payload = get_bookmark_from_request(
+            self.request, loadjson=True
+        )
+        alsoProvides(self.request, IDisableCSRFProtection)
+        # payload = self.request.form.get("payload", "{}")
+        bookmarks = Bookmarks()
+        bookmark = bookmarks.update(owner, uid, group, queryparams, payload)
+        if not bookmark:
+            # be kind
+            bookmark = bookmarks.add(owner, uid, group, queryparams, payload)
+        self.request.response.setStatus(201)
+        return bookmark_dict_to_json_dict(bookmark)
