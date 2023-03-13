c996a74237a5e729448ccc73b3a3e97a60da5fd7
laszlocseh <csehlaszlo91@gmail.com>
2023-03-13 16:04:46 +0200

    Added scraper to setup case studies from nwrm

---
+++ freshwater/content/upgrades/to14.py
@@ -0,0 +1,12 @@
+''' Upgrade to 14 '''
+
+from plone import api
+
+def run_upgrade(setup_context):
+    """ Run upgrade to 14
+    """
+
+    pl = api.portal.get_tool('portal_languages')
+    default_language = pl.getDefaultLanguage()
+    root = api.portal.get()
+    root.language = default_language
