d5d970d22f37cf0cca62e924dac29292dc19da49
kreafox <krisztina.n.elekes@gmail.com>
2024-01-12 14:55:05 +0200

    Pylint

--- freshwater/content/upgrades/to19.py
+++ freshwater/content/upgrades/to19.py
@@ -2,15 +2,17 @@
 
 import logging
 from plone import api
+
 # pylint: disable = C0412
 
+
 def run_upgrade(context):
-    """ Run upgrade to 19
-        Set autoscale on Tableau visualization
+    """Run upgrade to 19
+    Set autoscale on Tableau visualization
     """
 
-    catalog = api.portal.get_tool('portal_catalog')
-    brains = catalog.unrestrictedSearchResults(portal_type='visualization_tableau')
+    catalog = api.portal.get_tool("portal_catalog")
+    brains = catalog.unrestrictedSearchResults(portal_type="visualization_tableau")
 
     for brain in brains:
         obj = brain.getObject()
