34adab04ad591f8a30a25abbd010fea01848b390
Miu Razvan <miu.razvan28@gmail.com>
2024-04-24 09:45:57 +0300

    fix docstring

--- freshwater/content/services/types/get.py
+++ freshwater/content/services/types/get.py
@@ -1,3 +1,6 @@
+"""
+TypesGet
+"""
 from plone.restapi.services.types.get import (
     check_security,
     TypesGet as BaseTypesGet
@@ -45,21 +48,51 @@ plone_site_schema = {
 
 @implementer(IPublishTraverse)
 class TypesGet(BaseTypesGet):
+    """
+    A custom implementation of the TypesGet service for freshwater content.
+    """
+
     def __init__(self, context, request):
         super().__init__(context, request)
         self.params = []
 
     def publishTraverse(self, request, name):
+        """
+        Handles traversal of the object hierarchy.
+
+        This method is called when a URL path is traversed and determines how the object hierarchy is navigated.
+
+        :param request: The request object.
+        :param name: The name of the path segment being traversed.
+        :return: The updated object after traversal.
+        """
         # Treat any path segments after /@types as parameters
         self.params.append(name)
         return self
 
     def reply(self):
+        """
+        This method handles the reply logic for the 'get' service type.
+
+        If the number of parameters is 1 and the parameter is 'Plone Site', it calls the 'reply_for_plone_site' method.
+        Otherwise, it calls the parent class's 'reply' method.
+
+        Returns:
+            The response from the appropriate reply method.
+        """
         if len(self.params) == 1 and self.params[0] == 'Plone Site':
             return self.reply_for_plone_site()
         return super().reply()
 
     def reply_for_plone_site(self):
+        """
+        Returns the JSON schema for the Plone site.
+
+        This method checks the security of the context and sets the content type to "application/json+schema".
+        It then returns the JSON schema for the Plone site.
+
+        :return: The JSON schema for the Plone site.
+        """
         check_security(self.context)
         self.content_type = "application/json+schema"
         return plone_site_schema
