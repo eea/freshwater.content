648f6af156f4c3d16d1b3f9a85c36df5fb463416
Miu Razvan <miu.razvan28@gmail.com>
2024-04-24 09:59:19 +0300

    fix

--- freshwater/content/services/types/get.py
+++ freshwater/content/services/types/get.py
@@ -53,18 +53,12 @@ class TypesGet(BaseTypesGet):
     """
 
     def __init__(self, context, request):
-        super().__init__(context, request)
+        super(TypesGet, self).__init__(context, request)
         self.params = []
 
     def publishTraverse(self, request, name):
         """
         Handles traversal of the object hierarchy.
-
-        This method is called when a URL path is traversed and determines how the object hierarchy is navigated.
-
-        :param request: The request object.
-        :param name: The name of the path segment being traversed.
-        :return: The updated object after traversal.
         """
         # Treat any path segments after /@types as parameters
         self.params.append(name)
@@ -73,12 +67,6 @@ class TypesGet(BaseTypesGet):
     def reply(self):
         """
         This method handles the reply logic for the 'get' service type.
-
-        If the number of parameters is 1 and the parameter is 'Plone Site', it calls the 'reply_for_plone_site' method.
-        Otherwise, it calls the parent class's 'reply' method.
-
-        Returns:
-            The response from the appropriate reply method.
         """
         if len(self.params) == 1 and self.params[0] == 'Plone Site':
             return self.reply_for_plone_site()
@@ -87,11 +75,6 @@ class TypesGet(BaseTypesGet):
     def reply_for_plone_site(self):
         """
         Returns the JSON schema for the Plone site.
-
-        This method checks the security of the context and sets the content type to "application/json+schema".
-        It then returns the JSON schema for the Plone site.
-
-        :return: The JSON schema for the Plone site.
         """
         check_security(self.context)
         self.content_type = "application/json+schema"
