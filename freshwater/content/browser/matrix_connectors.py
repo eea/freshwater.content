b0b99de7f89b467170a917c5817629e7079c4dd5
vladcalin-edw <calin.vlad@eaudeweb.ro>
2025-07-24 11:24:08 +0300

    visualization`s connectors and files

--- freshwater/content/browser/matrix_connectors.py
+++ freshwater/content/browser/matrix_connectors.py
@@ -4,8 +4,9 @@ from Products.Five.browser import BrowserView
 from plone import api
 
 """
-TODO: [x] Get all visualizations (Too much data to get siblings, think about it)
-TODO: [] Get all connectors / avoid sandbox
+TODO: [x] Get all visualizations and related connectors
+TODO: [] Handle .csv connectors
+TODO: [] Map connectors to visualizations
 TODO: [] Determine relationship between visualizations and connectors
 TODO: [] Determine status of visualizations and used pages
 """
@@ -14,20 +15,34 @@ class MatrixConnectors(BrowserView):
     def __call__(self):
         visualizations_result = api.content.find(portal_type="visualization")
         connectors_result = api.content.find(portal_type="discodataconnector")
+        files_result = api.content.find(portal_type="File")
         visualizations = []
-        connectors = []
+        connectors = {}
+        files = {}
 
         for brain in connectors_result:
             obj = brain.getObject()
 
-            connectors.append({
+            connectors[obj.id] = {
                 "id": obj.id,
                 "uid": obj.UID(),
                 "url": brain.getURL(),
                 "path": brain.getPath(),
                 "title": obj.Title(),
                 "description": obj.Description()
-            })
+            }
+
+        for brain in files_result:
+            obj = brain.getObject()
+
+            files[obj.id] = {
+                "id": obj.id,
+                "uid": obj.UID(),
+                "url": brain.getURL(),
+                "path": brain.getPath(),
+                "title": obj.Title(),
+                "description": obj.Description()
+            }
 
         for brain in visualizations_result:
             obj = brain.getObject()
@@ -35,8 +50,7 @@ class MatrixConnectors(BrowserView):
 
             visualization = getattr(obj, 'visualization', None)
             provider_url = visualization.get('provider_url')
-            filename = os.path.basename(provider_url)
-            name = os.path.splitext(filename)[0]
+            name = os.path.basename(provider_url)
 
             visualizations.append({
                 "id": obj.id,
@@ -46,8 +60,9 @@ class MatrixConnectors(BrowserView):
                 "title": obj.Title(),
                 "description": obj.Description(),
                 "provider_url": provider_url,
-                "connector": name
+                "connector": connectors.get(name, None),
+                "file": files.get(name, None)
             })
 
         self.request.response.setHeader("Content-Type", "application/json")
-        return json.dumps({"visualizations": visualizations, "connectors": connectors})
+        return json.dumps(visualizations)
